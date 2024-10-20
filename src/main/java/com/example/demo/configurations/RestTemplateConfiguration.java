f930bf93c70025073ff39c85f96cb53ff1921ecb
sandeepmishramca <sandeepmishramca@gmail.com>
2024-10-20 15:03:17 +0530

    added docker

--- src/main/java/com/example/demo/configurations/RestTemplateConfiguration.java
+++ src/main/java/com/example/demo/configurations/RestTemplateConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.demo.configurations;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfiguration {
     @Bean
+//    @LoadBalanced //Client side load balancing
     public RestTemplate restTemplate() {
         return new RestTemplate(new HttpComponentsClientHttpRequestFactory());
 
